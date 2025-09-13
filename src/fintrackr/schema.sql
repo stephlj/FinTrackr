1d4fd58b1893969d65cfecf6723212b9b6176680
Stephanie Johnson <johnson.stephanie076@gmail.com>
2025-09-12 18:07:51 -0700

    Revised schema

--- src/fintrackr/schema.sql
+++ src/fintrackr/schema.sql
@@ -6,45 +6,33 @@ foreign keys: x_id, using singular for x
 
 */
 
-CREATE TYPE frequency AS ENUM(
+CREATE TYPE recurrance AS ENUM(
     "irregular", "monthly", "annual"
-)
+);
 
-
-CREATE TABLE expenses (
+CREATE TABLE transactions(
     id SERIAL PRIMARY KEY,
     posted_date date NOT NULL,
     amount money NOT NULL,
     source text /* e.g., "main card", "primary checking" */
 );
 
-CREATE TABLE incomes(
+CREATE TABLE categorization(
     id SERIAL PRIMARY KEY,
-    posted_date date NOT NULL,
-    amount money NOT NULL,
-    source text /* e.g. "gift", "cap gains" */
+    username text NOT NULL,
+    categorization_name text /* if a user wants more than one categorization scheme */ 
 );
 
 CREATE TABLE categories(
     id SERIAL PRIMARY KEY,
-    category_label_id integer REFERENCES category_labels(id),
-    recurrance frequency DEFAULT 1
+    label text NOT NULL, /* e.g. "groceries" */
+    recurrance frequency DEFAULT 1,
+    categorization_id integer NOT NULL REFERENCES categorization(id),
+    other text /* optional additional super-category (e.g. "discretionary") */
 );
 
-CREATE TABLE category_labels(
+CREATE TABLE transactions_categories_xref(
     id SERIAL PRIMARY KEY,
-    username text NOT NULL,
-    label text NOT NULL /* e.g. "groceries" */
-);
-
-CREATE TABLE expenses_categories_xref(
-    id SERIAL PRIMARY KEY,
-    expense_id integer NOT NULL REFERENCES expenses(id),
-    category_id integer NOT NULL REFERENCES categories(id),
-);
-
-CREATE TABLE incomes_categories_xref(
-    id SERIAL PRIMARY KEY,
-    income_id integer NOT NULL REFERENCES incomes(id),
+    transaction_id integer NOT NULL REFERENCES transactions(id),
     category_id integer NOT NULL REFERENCES categories(id),
 );
