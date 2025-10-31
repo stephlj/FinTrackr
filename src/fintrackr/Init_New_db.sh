17f9e51c8976163f9b1a68f8be868a5e85ae4880
Stephanie Johnson <johnson.stephanie076@gmail.com>
2025-10-31 07:53:12 -0700

    Added logging to init_db

--- src/fintrackr/Init_New_db.sh
+++ src/fintrackr/Init_New_db.sh
@@ -3,6 +3,7 @@
 # Initialize FinTrackr db
 # Admin should do this once, users never; can also be called by tests
 # Arg is db name. createdb will fail if db already exists
+# TODO pass the error out if creation fails
 
 createdb $1
 psql $1 < src/fintrackr/schema.sql
--- tests/test_db.py
+++ tests/test_db.py
@@ -5,33 +5,33 @@
 #
 # Copyright (c) 2025 Stephanie Johnson
 
-import psycopg, unittest, os, subprocess
+import psycopg, unittest, subprocess
+
+from fintrackr.init_db import init_db
 
 class TestDBSetup(unittest.TestCase):
     @classmethod
-    def setupClass(self):
-        # make a test db
+    def setUpClass(self):
+        # Make a test db, in the process also tests init_db.
         # TODO later use mocking instead of a real db
-
-        path_to_initscript = os.path.join(os.getcwd(),"src","fintrackr","Init_New_db.sh")
         
-        exit_code = subprocess.run([path_to_initscript,"test"], shell=True)
-        self.assertNotEqual(exit_code,0, "Failed to create testing db")
+        init_db(db_name="test")
 
-        self.conn = psycopg.connect("dbname = test user=test_user")
+        #self.conn = psycopg.connect("dbname=test user=test_user")
+        self.conn = psycopg.connect("dbname=test")
 
     @classmethod
     def tearDownClass(self):
-        # delete testing db
-        remove_db = subprocess.run(["dropdb","test"],capture_output=True)
-        # TODO Check {remove_db.stdout}, {remove_db.stderr}
-
+        # close connection and delete testing db
+        self.conn.close()
+        exit_code = subprocess.run(["dropdb","test"])
+        assert exit_code.returncode==0, "Failed to remove testing db"
 
     def test_schema_match(self):
         # Check that the schema of the real db matches this test db 
         # we just made from the schema file
         pass
 
-    def test_add_transactions(self):
-        # Will test add_transactions method TODO
-        pass
+    # def test_add_transactions(self):
+    #     # Will test add_transactions method TODO
+    #     pass
