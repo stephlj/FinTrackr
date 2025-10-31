25fb29ceeb55596f7ad41df5e56b592bd9976d2a
Stephanie Johnson <johnson.stephanie076@gmail.com>
2025-10-31 16:06:51 -0700

    Add connection to db with pw

--- tests/test_db.py
+++ tests/test_db.py
@@ -16,12 +16,13 @@ class TestDBSetup(unittest.TestCase):
         # TODO later use mocking instead of a real db
         
         self.test_db_name = "test"
-        self.test_owner = "test_admin" # use default pw
+        self.test_owner = "test_admin"
+        self.owner_pw = "test_admin"
 
-        init_db(db_name=self.test_db_name,owner=self.test_owner)
+        init_db(db_name=self.test_db_name,owner=self.test_owner, pw=self.owner_pw)
 
         # TODO connect as something other than owner
-        self.conn = psycopg.connect(f"dbname={self.test_db_name} user={self.test_owner}")
+        self.conn = psycopg.connect(f"dbname={self.test_db_name} user={self.test_owner} password={self.owner_pw}")
 
     @classmethod
     def tearDownClass(self):
