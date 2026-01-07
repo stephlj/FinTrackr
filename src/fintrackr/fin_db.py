cd9cb2028c37f6adfb46fa749980bcc89a171f09
Stephanie Johnson <johnson.stephanie076@gmail.com>
2026-01-06 19:29:10 -0800

    Add add_transactions function and test

--- src/fintrackr/fin_db.py
+++ src/fintrackr/fin_db.py
@@ -13,7 +13,7 @@ Copyright (c) 2025 Stephanie Johnson
 import psycopg
 import logging
 import os
-import re
+from datetime import date
 
 import pandas as pd # temporary, till I move some logic elsewhere
 
@@ -126,14 +126,18 @@ class FinDB:
         """
         Returns the result of a query to the database.
 
-        This feels like not great re: security/"little Bobby Tables" situations,
-        so I'm forcing the query to be a SELECT statement only for now. (Not that
-        that prevents Little Bobby Tables anyway ... TODO switch to parameterized SQL)
+        This is not great re: security/"little Bobby Tables" situations.
+        TODO use parameterized SQL to fix.
+
+        TODO Should I have different methods for SELECT (doesn't alter db data)
+        vs INSERT (does alter db data)? And make INSERT an internal method perhaps?
 
         Parameters
         ----------
         query : str
-            SELECT statement to execute
+            SELECT or INSERT statement to execute
+            (something where the return should be the result of a fetchall, rather 
+            than a status message)
 
         Returns
         -------
@@ -144,10 +148,6 @@ class FinDB:
         """
 
         response = None
-
-        if query[0:6] != "SELECT".upper(): # goofy!
-            logger.error("FinDB.execute_query only accepts SELECT statements")
-            return response
 
         with self._conn.cursor() as curs: 
             logger.info(f"Executing query {query}")
@@ -177,7 +177,7 @@ class FinDB:
         """
 
         create_staging = " CREATE TABLE IF NOT EXISTS staging( " \
-            " Date date, Amount money, Description text); "
+            " posted_date date, amount money, description text); "
         
         # TODO move this to business logic layer
         # Check that the input conforms to expectations
@@ -210,31 +210,76 @@ class FinDB:
 
         return len(rows_after)-len(rows_before)
 
-    # def add_metadata(self, ...)
-    #     meta_query = "INSERT INTO data_load_metadata (date_added, username, source) VALUES () RETURNING id;"
-    #     meta_id = self._execute_query(meta_query, ("data_load_metadata",))
+    def add_transactions(self, path_to_source_file: str, source_info: str) -> None:
+        """
+        Load transactions from a file and log the addition of these transactions
+        in the data_load_metadata table.
 
-    # def add_transactions(self, path_to_transactions: str) -> None:
+        Only new transactions are added; duplicates (which have identity across the 3
+        input columns of Date, Amount, and Description with an existing transaction row)
+        are ignored.
 
-    #     self._loadtransactions(path_to_transactions=path_to_transactions)
+        Parameters
+        ----------
+        path_to_source_file: str
+            Path to a csv where every row is a transaction.
+        source_info: str
+            Are these transactions from credit card, checking account, etc
+            This is the "name" field in the data_sources table.
+            It will be added if it doesn't already exist.
 
-    #     self.add_metadata() # get FK
+        Returns
+        -------
+        int
+            Number of transactions added.
+        """
+        num_new_transactions = 0
 
-    #     trans_query = "INSERT INTO transactions (poasted_date, amount, description, metadatum_id) VALUES ()"
-    #     success = self.cur.execute(trans_query, ("transactions",))
+        num_staged_transactions = self.load_transactions(path_to_transactions = path_to_source_file)
 
-    #     The SQL for what I want to do here: 
-#     with joined as (
-# 	select	s.*
-# 	from	staging s
-# 	left join destination d on
-# 		d.colA = s.colA and
-# 		d.colB = s.colB
-# 	where	d.id is null
-# )
-# insert into destination (colA, colB)
-# select	colA, colB
-# from	joined
-# returning id;
+        if num_staged_transactions == 0:
+            logger.info("No transactions loaded from source file to staging table; no transactions will be added")
+            return num_new_transactions
+        
+        # Get id for this source_info, if it exists
+        # This can be done in one query but race conditions can occur
+        source_info_id = self.execute_query(f"SELECT id FROM data_sources WHERE name='{source_info}';")
+        if source_info_id is None:
+           logger.info(f"Data source {source_info} doesn't exist; adding to data_sources table")
+           source_info_id = self.execute_query(f"INSERT INTO data_sources (name) VALUES ('{source_info}' RETURNING id);")
+           if source_info_id is None:
+               raise ValueError("Could not insert new data source in data_sources table")
+        
+        transactions_query = f"WITH joined AS ( " \
+            "    SELECT	s.* " \
+            "    FROM	staging s " \
+            "    LEFT JOIN transactions t ON " \
+            "        t.posted_date = s.posted_date AND " \
+            "        t.amount = s.amount AND " \
+            "        t.description = s.description " \
+            "    WEHRE	t.id IS NULL " \
+            "), " \
+            "meta AS ( " \
+            "    INSERT INTO data_load_metadata " \
+            "        (date_added, username, source, data_source_id) " \
+            "    VALUES ('{date.today}', '{self.user}', '{path_to_source_file}', '{source_info}')" \
+            "    " \
+            "    RETURNING id " \
+            ") " \
+            "INSERT INTO transactions (posted_date, amount, description, metadatam_id) " \
+            "SELECT	posted_date, amount, description, meta.id " \
+            "FROM joined, meta; " \
+            
+        num_new_transactions = self.execute_query(transactions_query) # This returns the result of the final INSERT statement
+        
+        # Drop staging table
+        self._execute_action("DROP TABLE staging;")
+
+        return num_new_transactions
     
-    #     # Remove all rows from staging table or drop table;
+    # def update_categorizations(self, ...):
+    #     """
+    #     Should probably have a separate function that gets uncategorized transactions?
+    #     """
+
+
--- tests/test_db.py
+++ tests/test_db.py
@@ -38,8 +38,12 @@ class TestDBSetup(unittest.TestCase):
                  path_to_config=path_to_config
         )
 
-        # cls.conn = psycopg.connect(f"dbname={cls.test_db_name} user={cls.user} password={cls.user_pw} host='localhost'")
         cls.FinDB = FinDB(user=cls.user, pw=cls.user_pw, db_name=cls.test_db_name)
+
+        # Some test fixtures shared by multiple tests:
+        cls.path_to_test_transactions = os.path.join(os.getcwd(),"tests","data","test_data_cc.csv")
+        cls.transactions_to_add = pd.read_csv(cls.path_to_test_transactions, header=None)
+        cls.source_info = "cc"
 
     @classmethod
     def tearDownClass(cls):
@@ -58,30 +62,37 @@ class TestDBSetup(unittest.TestCase):
 
     def test_execute_query(self):
         # Test cases:
-        # - not a SELECT statement (eg, insert)
         # - trying to query a table that doesn't exist in the db
         pass
     
     def test_load_transactions(self):
-        path_to_test_transactions = os.path.join(os.getcwd(),"tests","data","test_data_cc.csv")
-        transactions_to_add = pd.read_csv(path_to_test_transactions, header=None)
+        # Also contains an implicit test for execute_query which is not ideal ... 
 
         # load_transactions adds rows to a staging table that should be empty at start
-        num_rows_added = self.FinDB.load_transactions(path_to_transactions=path_to_test_transactions)
-        self.assertEqual(num_rows_added, transactions_to_add.shape[0], "Rows added to staging table does not match file")
-        element_to_match = str(transactions_to_add.iloc[1,1])
+        num_rows_added = self.FinDB.load_transactions(path_to_transactions=self.path_to_test_transactions)
+        self.assertEqual(num_rows_added, self.transactions_to_add.shape[0], "Rows added to staging table does not match file")
+        element_to_match = str(self.transactions_to_add.iloc[1,1])
         element_to_match = element_to_match[0] + "$" + element_to_match[1:] + "0"
         self.assertEqual(element_to_match, 
-                         self.FinDB.execute_query(f"SELECT Amount FROM staging WHERE Description='Concert tickets';")[0][0], 
+                         self.FinDB.execute_query(f"SELECT amount FROM staging WHERE description='Concert tickets';")[0][0], 
                          "Data were scrambled when copied into staging"
                          )
 
         path_to_too_many_columns = os.path.join(os.getcwd(),"tests","data","test_data_cc_wrongnumcols.csv")
-        num_rows_added = self.FinDB.load_transactions(path_to_transactions=path_to_too_many_columns)
-        self.assertEqual(num_rows_added, 0, "No rows should have been added, malformed input")
+        num_rows_added_2 = self.FinDB.load_transactions(path_to_transactions=path_to_too_many_columns)
+        self.assertEqual(num_rows_added_2, 0, "No rows should have been added, malformed input")
 
         # TODO test wrongtype - but that should go in BLL test anyway
     
     def test_add_transactions(self):
-    #     # Will test add_transactions method TODO
-         pass
+        # Add_transactions calls load_transactions (which we test separately above)
+         
+        # Staging will already contain the contents of this file:
+        # TODO what happens when I re-call load_transactions?
+        num_transactions_added = self.FinDb.add_transactions(
+            path_to_source_file = self.path_to_test_transactions, 
+            source_info = self.source_info
+            )
+        self.assertEqual(num_transactions_added, self.transactions_to_add.shape[0], "Not all transactions were added")
+
+        # TODO additional test for what happens when duplicates are attempted to add
